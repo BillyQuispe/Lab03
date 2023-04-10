6d116cc7a88e57b8c9c5944b3df7e90df99cde82
BillyQuispe <carlos.quispe.v@tecsup.edu.pe>
2023-04-09 22:05:50 -0500

    Lab03

---
+++ actividad06.js
@@ -0,0 +1,10 @@
+let nombrecompleto = "Juan Perez";
+let nombreCompleto = "Carlos Lara";
+console.log(nombrecompleto);
+console.log(nombreCompleto);
+
+let alnombreCompleto;
+let _nombreCompleto;
+let $nombreCompleto;
+
+let ruptura = 10;
